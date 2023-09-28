8f08c33110e6e11b374f95d1c924259d678b90d2
NyatiZw <appsmaid19@gmail.com>
2023-09-28 10:37:08 +0000

    fixing white space error

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,6 +3,7 @@
 0-pascal_triangle
 """
 
+
 def pascal_triangle(n):
     """
     Generates Pascal's triangle up to the nth row
@@ -20,7 +21,7 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         for j in range(1, i):
-            row.append(triangle[i - 1][j -1] + triangle[i - 1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)
         triangle.append(row)
 
