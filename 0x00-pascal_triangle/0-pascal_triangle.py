35f7781600927122814958621ad568bbc9fc2fc9
NyatiZw <appsmaid19@gmail.com>
2023-09-28 10:14:12 +0000

    pushing pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,14 +5,23 @@
 
 def pascal_triangle(n):
     """
-    returns list of lists of integer representing 
-    Pascal's triangle of n
+    Generates Pascal's triangle up to the nth row 
+
+
+    :param n: number of rows to generate
+    :type n: int
+    :return: A list of lists representing Pascal's triangle.
+    :rtype: list of lists of integers
     """
-    if int(n) <= 0:
+    if n <= 0:
         return []
-    else:
-        n += 1
 
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j -1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
 
-if __name__ == "__main__":
-    main()
+    return triangle
