ca5e6ed2d47b08fa519af7687df1965943138f9f
vagrant <vagrant@vagrant.vm>
2023-10-15 19:20:48 +0000

    push solution

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,36 @@
+#|/usr/bin/python3
+"""
+Method to calculate the fewest number of operations
+
+Args:
+    n: integer
+
+Returns:
+    integer value || 0
+"""
+
+import math
+
+
+def minOperations(n):
+    """
+    Calculate the fewest number of operations to achive a goal.
+
+    Args:
+    n (int): The target integer.
+
+    Returns:
+    int: The fewest number of operations.
+    """
+    if not isinstance(n, int) or n <=0:
+        return 0
+
+    operations = 0
+    while n > 1:
+        if n % 2 == 0:
+            n //= 2
+        else:
+            n -= 1
+        operations += 1
+
+    return operations
