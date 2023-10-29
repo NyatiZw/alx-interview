c914f6006eaa55644dffe1a0a19921d2c96925f7
vagrant <vagrant@vagrant.vm>
2023-10-29 14:04:12 +0000

    fixing regular expression

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,35 @@
 #!/usr/bin/python3
+""" Script that reads stdin and computes metrics"""
+
+
 import sys
+import re
+from collections import defaultdict
 
-def main():
-    total_size = 0
-    status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-    line_number = 0
+log_format = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[.+\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)')
 
-    try:
-        for line in sys.stdin:
-            line = line.strip()
-            if is_valid_line(line):
-                line_number += 1
-                _, _, _, status_code, file_size = parse_line(line)
-                total_size += file_size
-                if status_code in status_code_count:
-                    status_code_count[status_code] += 1
+total_size = 0
+status_code_count = defaultdict(int)
 
-            if line_number % 10 == 0:
-                print_statistics(total_size, status_code_count)
+line_count = 0
 
-    except KeyboardInterrupt:
-        print_statistics(total_size, status_code_count)
+try:
+    for line in sys.stdin:
+        match = log_format.match(line)
+        if match:
+            ip, status_code, file_size = match.groups()
+            total_size += int(file_size)
+            status_code_count[status_code] += 1
+            line_count += 1
 
-def is_valid_line(line):
-    # Check if a line matches the expected format
-    return line.count('"') >= 2 and line.count('[') >= 1
+            if line_count % 10 == 0:
+                print(f"Total file size: {total_size}")
+                for code in sorted(status_code_count.keys()):
+                    print(f"{code}: {status_code_count[code]}")
 
-def parse_line(line):
-    # Parse the line and extract status code and file size
-    parts = line.split()
-    status_code = int(parts[-2])
-    file_size = int(parts[-1])
-    return status_code, file_size
+except KeyboardInterrupt:
+    pass
 
-def print_statistics(total_size, status_code_count):
-    print(f"Total file size: File size: {total_size}")
-    for status_code, count in sorted(status_code_count.items()):
-        if count > 0:
-            print(f"{status_code}: {count}")
-
-if __name__ == "__main__":
-    main()
+print(f"Total file size: {total_size}")
+for code in sorted(status_code_count.keys()):
+    print(f"{code}: {status_code_count[code]}")
