2ec6da08a1e1466546137f1f1e0b611bd1687960
vagrant <vagrant@vagrant.vm>
2023-10-29 06:03:55 +0000

    editing file names

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-""" Method to validate UTF-8 encoding """
-
+"""Method to validate UTF-8 encoding"""
 
 def validUTF8(data):
-    """ Function to check if a byte is a valid UTF-8 """
+    """Function to check if a byte is a valid UTF-8"""
     def is_continuation(byte):
         return (byte & 0b11000000) == 0b10000000
 
@@ -11,7 +10,7 @@ def validUTF8(data):
     while i < len(data):
         byte = data[i]
 
-        """Check the number of byte in the current character"""
+        """Check the number of bytes in the current character"""
         if (byte & 0b10000000) == 0:
             i += 1
         elif (byte & 0b11100000) == 0b11000000:
@@ -19,26 +18,25 @@ def validUTF8(data):
                 i += 2
             else:
                 return False
-        elif (byte & 0b1110000) == 0b11100000:
+        elif (byte & 0b11110000) == 0b11100000:
             if (
-                    i + 2 < len(data)
-                    and is_continuation(data[i + 1])
-                    and is_continuation(data[i + 2])
+                i + 2 < len(data)
+                and is_continuation(data[i + 1])
+                and is_continuation(data[i + 2])
             ):
                 i += 3
             else:
                 return False
         elif (byte & 0b11111000) == 0b11110000:
             if (
-                    i + 3 < len(data)
-                    and is_continuation(data[i + 1])
-                    and is_continuation(data[i + 2])
-                    and is_continuation(data[i + 3])
+                i + 3 < len(data)
+                and is_continuation(data[i + 1])
+                and is_continuation(data[i + 2])
+                and is_continuation(data[i + 3])
             ):
                 i += 4
             else:
                 return False
-            else:
-                return False
 
-        return True
+    return True
+
