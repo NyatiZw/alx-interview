a64fbd55535bd526a13009a7ef059e2250b8c656
vagrant <vagrant@vagrant.vm>
2023-10-29 06:31:12 +0000

    fixing pycodestyle

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,43 +1,43 @@
 #!/usr/bin/python3
 import sys
 
-
 def main():
     total_size = 0
-    status_code_count = {}
+    status_code_count = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
+    line_number = 0
 
     try:
-        for line_number, line in enumerate(sys.stdin, start=1):
+        for line in sys.stdin:
+            line = line.strip()
+            if is_valid_line(line):
+                line_number += 1
+                _, _, _, status_code, file_size = parse_line(line)
+                total_size += file_size
+                if status_code in status_code_count:
+                    status_code_count[status_code] += 1
+
             if line_number % 10 == 0:
                 print_statistics(total_size, status_code_count)
 
-            try:
-                ip, date, request, status_code, file_size = parse_line(line)
-                total_size += file_size
-                if status_code in [200, 301, 400, 401, 403, 404, 405, 500]:
-                    status_code_count[status_code] = \
-                            status_code_count.get(status_code, 0) + 1
-            except ValueError:
-                continue
     except KeyboardInterrupt:
         print_statistics(total_size, status_code_count)
 
+def is_valid_line(line):
+    # Check if a line matches the expected format
+    return line.count('"') >= 2 and line.count('[') >= 1
 
 def parse_line(line):
+    # Parse the line and extract status code and file size
     parts = line.split()
-    if len(parts) == 10:
-        ip, date, request, status_code, file_size = \
-                parts[0], parts[3], parts[5], int(parts[8]), int(parts[9])
-        return ip, date, request, status_code, file_size
-    else:
-        raise ValueError("Invalid line format")
-
+    status_code = int(parts[-2])
+    file_size = int(parts[-1])
+    return status_code, file_size
 
 def print_statistics(total_size, status_code_count):
     print(f"Total file size: File size: {total_size}")
-    for status_code in sorted(status_code_count.keys()):
-        print(f"{status_code}: {status_code_count[status_code]}")
-
+    for status_code, count in sorted(status_code_count.items()):
+        if count > 0:
+            print(f"{status_code}: {count}")
 
 if __name__ == "__main__":
     main()
