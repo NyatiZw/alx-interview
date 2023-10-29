900fbb2d92b8b85e5177ef0c48fc5a17752cb67a
vagrant <vagrant@vagrant.vm>
2023-10-29 14:21:18 +0000

    fixing regular expression

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,7 +6,8 @@ import sys
 import re
 from collections import defaultdict
 
-log_format = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[.+\] "GET /projects/260 HTTP/1\.1" (\d+) (\d+)')
+log_format = re.compile(r'(\d+\.\d+\.\d+\.\d+) - \[.+\] '
+                        r'"GET /projects/260 HTTP/1\.1" (\d+) (\d+)')
 
 total_size = 0
 status_code_count = defaultdict(int)
@@ -23,13 +24,13 @@ try:
             line_count += 1
 
             if line_count % 10 == 0:
-                print(f"Total file size: {total_size}")
+                print(f"File size: {total_size}")
                 for code in sorted(status_code_count.keys()):
                     print(f"{code}: {status_code_count[code]}")
 
 except KeyboardInterrupt:
     pass
 
-print(f"Total file size: {total_size}")
+print(f"File size: {total_size}")
 for code in sorted(status_code_count.keys()):
     print(f"{code}: {status_code_count[code]}")
